d14a37b8fd7dc0a8c193abcfecc64902963632b4
NNahkan <hoksuz2424@gmail.com>
2023-01-06 00:26:09 -0500

    Fetching and rendering work but needs to clean

--- src/Components/Schedule/Schedule.jsx
+++ src/Components/Schedule/Schedule.jsx
@@ -10,22 +10,16 @@ export function Schedule({ state, dispatch }) {
   const [dataBase, setDataBase] = useState(null);
 
   const [field, setField] = useState(false); // kind a Id for edit part
-
   useEffect(() => {
-    async function getData() {
-      try {
-        await fetch("http://localhost:8000/api/v1/allData", {
-          method: "GET",
-        })
-          .then((response) => response.json())
-          .then((data) => setDataBase(data.data));
-      } catch (error) {
-        console.error(error.message);
-      }
-      console.log(dataBase);
-    }
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8000/api/v1/allData", {
+        method: "GET",
+      });
+      const newData = await response.json();
+      setDataBase(newData.data[0]);
+    };
 
-    getData("http://localhost:8000/api/v1/allData");
+    fetchData();
   }, []);
 
   const handleAdjust = (value, shift) => {
@@ -47,110 +41,121 @@ export function Schedule({ state, dispatch }) {
     "Sunday",
   ];
 
+  console.log(dataBase);
+
   const locations = state.locations;
+
+  //   fetch shorthands
+  // const preShifts = dataBase.preShifts
   return (
-    <div className={s.scheduleWrapper}>
-      <div>
-        <h1>Count : {state.count}</h1>
-        <button onClick={() => dispatch({ type: ACTIONS.ADD })}>Add</button>
-        <br />
-        <br />
-        <div style={{ marginBottom: "5rem" }}>
-          {Object.keys(locations).map((location) => (
-            <button onClick={() => setDepartment(location)}>
-              {location.toUpperCase()}
-            </button>
-          ))}
-        </div>
-        <table className={s.scheduleTable}>
-          <thead>
-            <tr>
-              <th></th>
-              {days && days.map((day) => <th>{day.toUpperCase()}</th>)}
-            </tr>
-          </thead>
-          <tbody>
-            {locations[department].map((employee) => {
-              const schedule = state.employees[employee].schedule;
-              return (
+    <>
+      {dataBase && (
+        <div className={s.scheduleWrapper}>
+          <div>
+            <h1>{state.count}</h1>
+            <button onClick={() => dispatch({ type: ACTIONS.ADD })}>Add</button>
+            <br />
+            <br />
+            <div style={{ marginBottom: "5rem" }}>
+              {Object.keys(locations).map((location) => (
+                <button onClick={() => setDepartment(location)}>
+                  {location.toUpperCase()}
+                </button>
+              ))}
+            </div>
+            <table className={s.scheduleTable}>
+              <thead>
                 <tr>
-                  <td>{employee.toUpperCase()}</td>
-                  {Object.keys(schedule).map((day) => (
-                    <td className={s.shiftWrapper}>
-                      {Object.keys(schedule[day]).length === 0 ? (
-                        <>
-                          <button
-                            onClick={() => {
-                              dispatch({
-                                type: ACTIONS.SHIFT,
-                                payload: {
-                                  person: employee,
-                                  shift: preTime,
-                                  day: day,
-                                  department: department,
-                                },
-                              });
-                            }}
-                          >
-                            Apply
-                          </button>
-                        </>
-                      ) : (
-                        <>
-                          {field === `${employee}${day}` ? (
-                            <input
-                              value={shift}
-                              onChange={(e) => setShift(e.target.value)}
-                              type="text"
-                            />
+                  <th></th>
+                  {days && days.map((day) => <th>{day.toUpperCase()}</th>)}
+                </tr>
+              </thead>
+              <tbody>
+                {locations[department].map((employee) => {
+                  const schedule = state.employees[employee].schedule;
+                  return (
+                    <tr>
+                      <td>{employee.toUpperCase()}</td>
+                      {Object.keys(schedule).map((day) => (
+                        <td className={s.shiftWrapper}>
+                          {Object.keys(schedule[day]).length === 0 ? (
+                            <>
+                              <button
+                                onClick={() => {
+                                  dispatch({
+                                    type: ACTIONS.SHIFT,
+                                    payload: {
+                                      person: employee,
+                                      shift: preTime,
+                                      day: day,
+                                      department: department,
+                                    },
+                                  });
+                                }}
+                              >
+                                Apply
+                              </button>
+                            </>
                           ) : (
                             <>
-                              {schedule[day].department === department ? (
-                                <>{schedule[day].shift}</>
+                              {field === `${employee}${day}` ? (
+                                <input
+                                  value={shift}
+                                  onChange={(e) => setShift(e.target.value)}
+                                  type="text"
+                                />
                               ) : (
-                                <>{schedule[day].department}</>
+                                <>
+                                  {schedule[day].department === department ? (
+                                    <>{schedule[day].shift}</>
+                                  ) : (
+                                    <>{schedule[day].department}</>
+                                  )}
+                                </>
                               )}
+                              <button
+                                onClick={() =>
+                                  handleAdjust(
+                                    `${employee}${day}`,
+                                    schedule[day].department === department
+                                      ? schedule[day].shift
+                                      : schedule[day].department
+                                  )
+                                }
+                                className={s.editButton}
+                              >
+                                x
+                              </button>
                             </>
                           )}
-                          <button
-                            onClick={() =>
-                              handleAdjust(
-                                `${employee}${day}`,
-                                schedule[day].department === department
-                                  ? schedule[day].shift
-                                  : schedule[day].department
-                              )
-                            }
-                            className={s.editButton}
-                          >
-                            x
-                          </button>
-                        </>
-                      )}
-                    </td>
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      <div className={s.preShiftsWrapper}>
-        <input type="time" />
+                        </td>
+                      ))}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+          <div className={s.preShiftsWrapper}>
+            <input type="time" />
 
-        <div>{preTime}</div>
-        <div>
-          <input onChange={(e) => setNewTime(e.target.value)} type="text" />
-          <button
-            onClick={() => dispatch({ type: ACTIONS.TIME, payload: newTime })}
-          >
-            Add new preTime
-          </button>
+            <div>{preTime}</div>
+            <div>
+              <input onChange={(e) => setNewTime(e.target.value)} type="text" />
+              <button
+                onClick={() =>
+                  dispatch({ type: ACTIONS.TIME, payload: newTime })
+                }
+              >
+                Add new preTime
+              </button>
+            </div>
+            {dataBase.preShifts.map((shift) => (
+              <button onClick={() => setPreTime(shift)}>{shift}</button>
+            ))}
+          </div>
         </div>
-        {state.preShifts.map((shift) => (
-          <button onClick={() => setPreTime(shift)}>{shift}</button>
-        ))}
-      </div>
-    </div>
+      )}
+    </>
   );
 }
